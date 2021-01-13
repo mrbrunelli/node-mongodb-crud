30a8382606cd3201fc0207c25a1bcb379aead918
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-01-13 19:42:07 -0300

    feat: add express server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const routes = require("./routes")
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use("/", routes)
+
+app.listen(PORT, () => {
+    console.log("Server running on port -> " + PORT)
+})
